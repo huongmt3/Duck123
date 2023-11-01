2bf221345c3643af01635902e11846d1e77dd930
huongmt3 <87848986+huongmt3@users.noreply.github.com>
2023-11-01 10:44:19 +0700

    Add files via upload

---
+++ dist/script.js
@@ -0,0 +1,30 @@
+const quack = new Audio(
+'https://freesound.org/data/previews/418/418509_5632532-lq.mp3');
+
+const duck = document.querySelector('.duck');
+const mute = document.querySelector('.mute');
+const play = () => {
+  quack.
+  play().
+  then(() => {
+    const dur = Math.floor(Math.random() * 5000);
+    duck.style.setProperty('--wing', dur);
+    if (quack.muted)
+    duck.style.setProperty('--wing', 0);
+    setTimeout(play, dur);
+  }).
+  catch(e => {
+    console.warn('Could not get quacking yet as no interaction 🦆 😭');
+  });
+};
+play();
+quack.muted = true;
+mute.addEventListener('click', () => {
+  quack.muted = !quack.muted;
+  if (quack.muted) {
+    mute.classList.add('mute--active');
+  } else {
+    mute.classList.remove('mute--active');
+    play();
+  }
+});
