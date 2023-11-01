d609cc174c67217d1d6797e9cf7775f9ea5a5dce
huongmt3 <87848986+huongmt3@users.noreply.github.com>
2023-11-01 10:43:00 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# Pure CSS Bobbing Ducko
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Hng-Ma-Thu/pen/WNPxOmP](https://codepen.io/Hng-Ma-Thu/pen/WNPxOmP).
+
---
+++ webapp.js
@@ -0,0 +1,46 @@
+const http = require('http');
+
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const html = require('html');
+const hbs = require('hbs');
+const handlebars = require('handlebars');
+
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/'));
+var {
+    allowInsecurePrototypeAccess
+} = require('@handlebars/allow-prototype-access');
+hbs.registerPartials(__dirname + '')
+
+const duck = require('./index.hbs');
+
+app.use('/duck', duck);
+
+app.get('/', async (req, res) => {
+    const template = handlebars.compile(fs.readFileSync('./index.hbs', 'utf-8'));
+    console.log(duck);
+    const result = template({
+        duck : './index.hbs'
+    }, {
+        allowProtoMethodsByDefault: false,
+        allowProtoPropertiesByDefault: false
+    })
+    res.render('./index.hbs', {
+        content: result
+    })
+})
+
+//fix cannot GET /
+app.get("/:universalURL", (req, res) => {
+    res.send("404 URL NOT FOUND");
+ });
+
+ /*===========================================
+ |                  PORT                     |
+ ============================================*/
+ //app.use('/', router);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT);
+console.debug('Web Server is listening at port' + PORT);
