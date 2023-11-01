a55ad881515e772160b22f894d8645211db604e9
huongmt3 <87848986+huongmt3@users.noreply.github.com>
2023-11-01 11:32:39 +0000

    alomost

---
+++ final.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const loading_router = express.Router();
+const handlebars = require('handlebars');
+const fs = require('fs');
+const hbs = require('hbs');
+const path = require('path');
+const multer = require('multer');
+const app = express();
+
+
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+
+final_router.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+/*==========================================
+ |                  INDEX                   |
+ ===========================================*/
+loading_router.get('/index', async (req, res) => {
+    const template = handlebars.compile(fs.readFileSync('./views/partials/final/final.hbs', 'utf-8'));
+    const result = template({
+        loading3: loadings
+    }, {
+        allowProtoMethodsByDefault: false,
+        allowProtoPropertiesByDefault: false
+
+    })
+    res.render('final/final.hbs', {
+        content: result
+    })
+})
+
+module.exports = final_router;
